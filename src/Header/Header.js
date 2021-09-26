e7afa98daf109bf38a12f4125ca98e1b4f84204a
jannatulmawyajoya <jannatuljoya228@gmail.com>
2021-09-26 17:54:23 +0600

    header added

---
+++ src/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div className="header">
+            <h1>WELCOME TO SUPERSTARS-CLUB</h1>
+            <h2>Total Budget: 100 Million</h2>
+            
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/data/superstars.js
@@ -0,0 +1,84 @@
+const superstars = [
+    {
+        key: 1,
+        name: 'Shah Rukh Khan',
+        age: 55,
+        bornIn: 'New Delhi',
+        proffesion: 'Actor',
+        img: 'https://ibb.co/3fjs1m7',
+        salary: 1200
+    },
+    {
+        key: 2,
+        name: 'Salman Khan',
+        age: 55,
+        bornIn: 'Indore',
+        proffesion: 'Actor',
+        img: 'https://ibb.co/LYY07SP',
+        salary: 1000
+    },
+    {
+        key: 3,
+        name: 'Hrithik Roshan',
+        age: 47,
+        bornIn: 'Bombay',
+        proffesion: 'Actor',
+        img: 'https://ibb.co/8b6rk1h',
+        salary: 1050
+    },
+    {
+        key: 4,
+        name: 'Ranveer Singh',
+        age: 36,
+        bornIn: 'Bombay',
+        proffesion: 'Actor',
+        img: 'https://ibb.co/wzN0b6p',
+        salary: 1100
+    },
+    {
+        key: 5,
+        name: 'Ranbir Kapoor',
+        age: 38,
+        bornIn: 'Mumbai',
+        proffesion: 'Actor',
+        img: 'https://ibb.co/hYQy6Dk',
+        salary: 1500
+    },
+    {
+        key: 6,
+        name: 'Kriti Sanon',
+        age: 31,
+        bornIn: 'New Delhi',
+        proffesion: 'Actress',
+        img: 'https://ibb.co/Lxkchh2',
+        salary: 1400
+    },
+    {
+        key: 7,
+        name: 'Alia Bhatt',
+        age: 28,
+        bornIn: 'Mumbai',
+        proffesion: 'Actress',
+        img: 'https://ibb.co/D4p049W',
+        salary: 1500
+    },
+    {
+        key: 8,
+        name: 'Deepika Padukone',
+        age: 35,
+        bornIn: 'Copenhagen',
+        proffesion: 'Actress',
+        img: 'https://ibb.co/wKyvpM0',
+        salary: 1600
+    },
+    {
+        key: 9,
+        name: 'Priyanka Chopra',
+        age: 39,
+        bornIn: 'Jamshedpur',
+        proffesion: 'Actress',
+        img: 'https://ibb.co/rHV88rW',
+        salary: 1400
+    }
+]
+
